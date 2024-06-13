c21cd1be5f5dc7c9e1a3a5b3f62e84c8a145a100
kiranzoro <kirankumar333444@gmail.com>
2024-06-13 23:14:18 +0530

    terraform

---
+++ functions/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "db"{
+    count = length(var.instance.names)
+    ami = "ami-031d574cddc5bb371"
+    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+    instance_type = "t2.micro"
+
+    tags = count.index[var.instance_names]
+}
+
+            #<resource-type>    <resource-name>
+resource "aws_security_group" "allow_ssh"{
+    name = "allow_ssh"
+    description = "allow ssh access"
+
+
+    
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" 
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+    tags = {
+        Name = "allow_ssh"
+        CreatedBy = "kiran"
+    }
+}
+
